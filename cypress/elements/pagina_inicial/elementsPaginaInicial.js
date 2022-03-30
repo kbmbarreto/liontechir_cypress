8497347af44bdb8b43ef14fefa7b55640879b5d5
cskmacedo <kleber.barreto@msn.com>
2022-03-30 20:40:51 -0300

    Casos de teste de login concluidos

---
+++ cypress/elements/pagina_inicial/elementsPaginaInicial.js
@@ -0,0 +1,8 @@
+const elementsPaginaInicial = {
+    ImpostoAPagar: {
+        titulo: 'body > div > div.styles__Main-sc-1ab02in-1.fOHRB > div > div.styles__Flex-sc-1zcmh4-7.' +
+            'efwfct > div.styles__Flex-sc-1zcmh4-7.cZfuxa > div.styles__Title-sc-1zcmh4-2.JKldh',
+    }
+}
+
+export default elementsPaginaInicial;
---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,37 @@
+import elementsLogin from "../elements/elementsLogin";
+import elementsPaginaInicial from "../elements/pagina_inicial/elementsPaginaInicial";
+
+describe('Testes da tela de login', () => {
+    before(() => {
+        cy.visit('https://painel.liontechir.com.br/')
+    })
+
+    it('Verificação dos componentes na tela', () => {
+        cy.get(elementsLogin.login.logoLiontech).should("exist")
+        cy.get(elementsLogin.login.usuario).should("exist")
+        cy.get(elementsLogin.login.senha).should("exist")
+        cy.get(elementsLogin.login.msgErroLogin).should("not.exist")
+        cy.get(elementsLogin.login.btnEsqueceuSuaSenha).should("contain", "Esqueceu sua senha?")
+    })
+
+    it('Tentativa de login com senha errada', () => {
+        cy.get(elementsLogin.login.usuario).clear().type('kleber.barreto@msn.com')
+        cy.get(elementsLogin.login.senha).clear().type('Xiforimpula2000@#$')
+        cy.get(elementsLogin.login.btnEntrar).should('contain', 'Entrar').click()
+        cy.get(elementsLogin.login.msgErroLogin).should('contain', 'Credenciais inválidas!')
+    })
+
+    it('Tentativa de login com e-mail errado', () => {
+        cy.get(elementsLogin.login.usuario).clear().type('akalistrombosio@msn.com')
+        cy.get(elementsLogin.login.senha).clear().type('Xiforimpula2000@#$')
+        cy.get(elementsLogin.login.btnEntrar).click()
+        cy.get(elementsLogin.login.msgErroLogin).should('contain', 'Credenciais inválidas!')
+    })
+
+    it('Tentativa de login com e-mail e senhas corretas', () => {
+        cy.get(elementsLogin.login.usuario).clear().type('kleber.barreto@msn.com')
+        cy.get(elementsLogin.login.senha).clear().type('OngaBongaSongaMonga')
+        cy.get(elementsLogin.login.btnEntrar).click()
+        cy.get(elementsPaginaInicial.ImpostoAPagar.titulo).should('contain', 'Imposto a Pagar')
+    })
+})
